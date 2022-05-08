d34c6517cba269dbad9de986e8ca81e900a783b2
Noura-karkoutly <noura.karkoutly@gmail.com>
2022-05-08 16:15:12 +0300

    first commit

---
+++ Users.js
@@ -0,0 +1,21 @@
+const users = [
+    {
+        id:1,
+        name:'Jhon',
+        email: 'john@gmail.com'
+    },
+    {
+        id: 2,
+        name :'Smith',
+        email: 'smith@gmail.com'
+    }
+    ,
+    {
+        id: 3,
+        name :'carl',
+        email: 'carl@gmail.com'
+    },
+];
+
+module.exports= users;
+
